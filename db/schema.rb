37a0e9180440b6a8d21d85d13c0bcc0b7bb8f1ac
COAB1 <carol.o.ag@gmail.com>
2020-09-09 22:25:17 -0300

    Create the model and do the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_215900) do
+ActiveRecord::Schema.define(version: 2020_09_10_012318) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_09_07_215900) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "workers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "social_name"
+    t.datetime "birth_date"
+    t.string "position"
+    t.string "section"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
