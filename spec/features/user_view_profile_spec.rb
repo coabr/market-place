5e887484042942f3bc0c3c55044730f0369d3add
COAB1 <carol.o.ag@gmail.com>
2020-09-15 22:53:58 -0300

    WIP:Create form to user fill profile reusing devise view and controller

--- spec/features/user_view_profile_spec.rb
+++ spec/features/user_view_profile_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 feature 'user fullfill its profile' do
   scenario 'successfully' do
-    user = User.create!(email: 'carol@procenge.com', password: '123456', full_name: 'Carolina Brito' , social_name: 'Carol B', birth_date: '25/06/1993', position: 'Software Engineer', department: 'Engineering')
-  
+    user = User.create!(email: 'carol@procenge.com', password: '123456')
     login_as(user, scope: :user)
+
     visit root_path
-    click_on 'Carol B'
+    click_on 'Profile'
+
+    fill_in 'Full name', with: 'Carolina Brito'
+    fill_in 'Social name', with: 'Carol B'
+    fill_in 'Date of birth', with: '06/25/1993'
+    fill_in 'Position', with: 'Software Engineer'
+    fill_in 'Sector', with: 'Engineering'
     expect(page).to have_content('Carolina Brito')
+    expect(page).to have_content('Carol B')
     expect(page).to have_content('06/25/1993')
     expect(page).to have_content('Software Engineer')
     expect(page).to have_content('Engineering')
